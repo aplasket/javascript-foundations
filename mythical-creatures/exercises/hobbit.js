7fb4000564c9ae0acda165026784463c1aaa3605
Ashley Plasket <ashley.plasket@gmail.com>
2023-12-20 17:51:06 -0500

    work on the hardest parts of hobbit file

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,9 +1,28 @@
+const createHobbit = (name, age = 0) => {
+  return {
+    name,
+    age,
+    acquaintances: [],
+  };
+}
 
+const meetPeople = (hobbit, people) => {
+  people.forEach(person => hobbit.acquaintances.push(person));
+  return hobbit;
+}
+
+const findFriends = (hobbit) => {
+  const friends = hobbit.acquaintances
+  .filter(person => person.relationship === 'friend')
+  .map(friend => friend.name);
+
+  return friends;
+}
 
 module.exports = {
-  // createHobbit, 
-  // celebrateBirthday, 
-  // getRing, 
-  // meetPeople, 
-  // findFriends
+  createHobbit,
+  // celebrateBirthday,
+  // getRing,
+  meetPeople,
+  findFriends
 }
